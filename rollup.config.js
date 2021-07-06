6d90fac498291022627fafdac5bea9919532880b
lroal <lars.roald@gmail.com>
2021-07-06 18:27:29 +0200

    simpler rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,11 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-// import json from '@rollup/plugin-json';
 
 export default {
 	input: 'index.js',
 	output: {
-		file: 'rdb.esm.js',
+		file: 'rdb-client.esm.js',
 		format: 'esm'
 	},
-	// onwarn: (message, warn) => {
-	// 	if (message.code === 'CIRCULAR_DEPENDENCY') return;
-	// 	warn(message);
-	// },
-	// plugins: [commonjs(), nodeResolve({browser: true, preferBuiltins: true}), json()]
 	plugins: [commonjs(), nodeResolve({browser: true})]
 };
