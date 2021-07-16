98ba1a2ff452754557663fd476ee46b63cb17fd0
Lars-Erik Roald <lars.roald@gmail.com>
2021-07-16 09:34:10 +0200

    fixed typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,39 +1,38 @@
-interface RdbClient {
-  beforeResponse(callback: (response: Response, options: ResponseOptions) => Promise<void> | void): void;
-  beforeRequest(callback: (request: RdbRequest) => Promise<RdbRequest> | RdbRequest | Promise<void> | void): void;
-  table<TTable>(path: string): TTable;
-  filter: Filter;
-  or(): Filter;
-  and(): Filter;
-  not(): Filter;
-  const(): Filter;
-  reactive(proxyMethod: (obj: any) => any): void;
+export = RdbClient;
+
+declare namespace RdbClient {
+  export function beforeRequest(callback: (response: Response, options: ResponseOptions) => Promise<void> | void): void;
+  export function beforeResponse(callback: (response: Response, options: ResponseOptions) => Promise<void> | void): void;
+  export function table<TTable>(path: string): TTable;
+  export const filter: Filter;
+  export function reactive(proxyMethod: (obj: any) => any): void;
+
+  export interface RawFilter {
+    sql: string | (() => string);
+    parameters?: any[];
+  }
+
+  export interface Filter extends RawFilter {
+    and(filter: Filter, ...filters: Filter[]): Filter;
+    or(filter: Filter, ...filters: Filter[]): Filter;
+    not(): Filter;
+  }
+
+  export interface ResponseOptions {
+    retry(): void;
+    attempts: number;
+  }
+
+  export type RdbRequest = {
+    method: string;
+    headers: Headers;
+    body: any;
+  }
+
+  export enum ConcurrencyValues {
+    Optimistic = 'optimistic',
+    SkipOnConflict = 'skipOnConflict',
+    Overwrite = 'overwrite'
+  }
+
 }
-
-
-export interface RawFilter {
-  sql: string | (() => string);
-  parameters?: any[];
-}
-
-export interface Filter extends RawFilter {
-  and(filter: Filter, ...filters: Filter[]): Filter;
-  or(filter: Filter, ...filters: Filter[]): Filter;
-  not(): Filter;
-}
-
-
-export interface ResponseOptions {
-  retry(): void;
-  attempts: number;
-}
-
-
-export interface RdbRequest {
-  method: string;
-  headers: Headers;
-  body: any;
-}
-
-declare const rdbClient : RdbClient;
-export default rdbClient;
