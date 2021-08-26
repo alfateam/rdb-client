4d07c76a2fe40fa586f3c4c1ae7f19c90f457f5b
Lars-Erik Roald <lars.roald@systor.st>
2021-08-26 15:39:23 +0200

    generate types

---
+++ bin/generate.mjs
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+import * as fs from 'fs';
+import * as util from 'util';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fileUrl from 'file-url';
+
+console.log(process.cwd());
+
+const cwd = process.cwd();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const writeFile = util.promisify(fs.writeFile);
+const appendFile = util.promisify(fs.appendFile);
+const copyFile = util.promisify(fs.copyFile);
+
+const indexSource = fileUrl(path.join(cwd, '/src/rdb/index.js'));
+const mod = await import(indexSource);
+const targetDir = path.join(__dirname, '../../node_modules/.rdb');
+const dsTarget = path.join(targetDir, '/index.d.ts');
+const coredsSource = path.join(__dirname, '/../core.d.ts');
+const coredsTarget = path.join(targetDir, '/core.d.ts');
+
+const appendix = `
+import {RdbClientBase, RawFilter, Filter, Concurrencies} from './core';
+export * from './core';
+
+export interface RdbClient extends RdbClientBase {
+    customer: CustomerTable;
+    order: OrderTable;
+}
+
+export interface RdbStatic {
+    (baseUrl: string): RdbClient;
+    (db: object): RdbClient;
+    filter: Filter;
+}      
+
+declare const rdbClient: RdbStatic;
+export default rdbClient;`
+
+
+if (!fs.existsSync(targetDir))
+    fs.mkdirSync(targetDir);
+await writeFile(dsTarget, appendix);
+
+await copyFile(coredsSource, coredsTarget);
+
+for(let name in mod) {
+    let ts = mod[name].ts(name);    
+    await appendFile(dsTarget, ts);
+}
