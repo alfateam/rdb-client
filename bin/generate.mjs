a4abc8a4ba0eac94d0335c01d298a5e9b4bfba4d
Lars-Erik Roald <lars.roald@systor.st>
2021-08-27 08:17:51 +0200

    generate package.json

--- bin/generate.mjs
+++ bin/generate.mjs
@@ -16,11 +16,16 @@ const appendFile = util.promisify(fs.appendFile);
 const copyFile = util.promisify(fs.copyFile);
 
 const indexSource = fileUrl(path.join(cwd, '/src/rdb/index.js'));
+const commonJs = `module.exports = require('${indexSource}')`;
+const moduleJs = `export * from '${indexSource}'`;
 const mod = await import(indexSource);
 const targetDir = path.join(cwd, '/node_modules/.rdb');
 const dsTarget = path.join(targetDir, '/index.d.ts');
 const coredsSource = path.join(__dirname, '/../core.d.ts');
 const coredsTarget = path.join(targetDir, '/core.d.ts');
+const commonJsTarget = path.join(targetDir, '/index.js');
+const moduleJsTarget = path.join(targetDir, '/index.ems.js');
+const packageJSONTarget = path.join(targetDir, '/package.json');
 
 const appendix = `
 import {RdbClientBase, RawFilter, Filter, Concurrencies} from './core';
@@ -40,10 +45,22 @@ export interface RdbStatic {
 declare const rdbClient: RdbStatic;
 export default rdbClient;`
 
+const packageJSON = `
+{
+    "name": ".a",
+    "version": "1.0.0",
+    "main": "index.js",
+    "module": "index.esm.js",
+}`;
+
 
 if (!fs.existsSync(targetDir))
     fs.mkdirSync(targetDir);
+await writeFile(packageJSONTarget, packageJSON);
+await writeFile(commonJs, commonJsTarget);
+await writeFile(moduleJs, moduleJsTarget);
 await writeFile(dsTarget, appendix);
+await writeFile()
 
 await copyFile(coredsSource, coredsTarget);
 
