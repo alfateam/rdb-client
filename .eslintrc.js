3bda24d6076dd264baa2ad35ffd60f64f3507ce0
Lars-Erik Roald <lars.roald@systor.st>
2021-09-01 10:24:07 +0200

    build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,39 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "es6": true,
-        "jest/globals": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": 2018
-    },
-    "ignorePatterns": ["*esm.js"],
-    "rules": {
-        "indent": ["error", "tab"],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"],
-        "space-before-function-paren": ["error", "never"],
-        "no-console": "off",
-        "no-debugger": "off",
-        "no-trailing-spaces": "error",
-        "no-underscore-dangle": 0,
-        "no-unused-vars": [2, { "args": "after-used", "ignoreRestSiblings": true }],
-        "space-before-function-paren": ["error", {
-            "anonymous": "never",
-            "named": "never",
-            "asyncArrow": "always"
-        }],
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error"
-    },
-    "plugins": ["jest", "@typescript-eslint"]
+	"env": {
+		"node": true,
+		"browser": true,
+		"es6": true,
+		"jest/globals": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"parserOptions": {
+		"sourceType": "module",
+		"ecmaVersion": 2018
+	},
+	"ignorePatterns": ["*esm.js"],
+	"rules": {
+		"indent": ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		"semi": ["error", "always"],
+		"no-console": "off",
+		"no-debugger": "off",
+		"no-trailing-spaces": "error",
+		"no-underscore-dangle": 0,
+		"no-unused-vars": [2, { "args": "after-used", "ignoreRestSiblings": true }],
+		"space-before-function-paren": ["error", {
+			"anonymous": "never",
+			"named": "never",
+			"asyncArrow": "always"
+		}],
+		"jest/no-disabled-tests": "warn",
+		"jest/no-focused-tests": "error",
+		"jest/no-identical-title": "error",
+		"jest/prefer-to-have-length": "warn",
+		"jest/valid-expect": "error"
+	},
+	"plugins": ["jest", "@typescript-eslint"]
 }
--- bin/build.js
+++ bin/build.js
@@ -10,54 +10,54 @@ let writeFile = util.promisify(fs.writeFile);
 run();
 
 async function run() {
-    let indexTs = await findIndexTs();
-    if (!indexTs)
-        return;
-    let clientDir = path.join(path.dirname(indexTs), '/client');
-    let nodeModules = findNodeModules({cwd: indexTs, relative: false})[0];
-    let outDir = path.join( nodeModules, '/.rdb-client');
-    let indexJsPath = compile(indexTs,  {outDir});
-    if (!indexJsPath)
-        return;
-    let indexJs = require(indexJsPath);
-    if ('default' in indexJs) 
-        indexJs = indexJs.default;
-    let defs = '';
-    for (let name in indexJs) {
-        let table = indexJs[name];
-        if (table.ts) {
-            defs+= table.ts(name);
+	let indexTs = await findIndexTs();
+	if (!indexTs)
+		return;
+	let clientDir = path.join(path.dirname(indexTs), '/client');
+	let nodeModules = findNodeModules({ cwd: indexTs, relative: false })[0];
+	let outDir = path.join(nodeModules, '/.rdb-client');
+	let indexJsPath = compile(indexTs, { outDir });
+	if (!indexJsPath)
+		return;
+	let indexJs = require(indexJsPath);
+	if ('default' in indexJs)
+		indexJs = indexJs.default;
+	let defs = '';
+	for (let name in indexJs) {
+		let table = indexJs[name];
+		if (table.ts) {
+			defs += table.ts(name);
 
-        }
-    }
-    let indexDts = path.join(clientDir, '/index.d.ts');
-    await writeFile(indexDts, getPrefixTs());
-    fs.appendFileSync(indexDts, defs);
-    fs.appendFileSync(indexDts, getRdbClientTs(indexJs));
-    await writeFile(path.join(clientDir, '/index.ts'), getClientIndexTs())
-    
+		}
+	}
+	let indexDts = path.join(clientDir, '/index.d.ts');
+	await writeFile(indexDts, getPrefixTs());
+	fs.appendFileSync(indexDts, defs);
+	fs.appendFileSync(indexDts, getRdbClientTs(indexJs));
+	await writeFile(path.join(clientDir, '/index.ts'), getClientIndexTs())
+
 }
 
 async function findIndexTs() {
-    let options = {
-        ignore: "**node_modules/**"
-    };
-    return new Promise(function(resolve, reject) {
-        glob("**/rdb/index.ts", options, async function (err, files) {
-            if (err)
-                reject(err);
-            else if (files.length === 0)
-                resolve();
-            else {
-                let file = path.join(process.cwd(), '/', files[0]);
-                resolve(file);
-            }
-          })
-      })      
+	let options = {
+		ignore: "**node_modules/**"
+	};
+	return new Promise(function (resolve, reject) {
+		glob("**/rdb/index.ts", options, async function (err, files) {
+			if (err)
+				reject(err);
+			else if (files.length === 0)
+				resolve();
+			else {
+				let file = path.join(process.cwd(), '/', files[0]);
+				resolve(file);
+			}
+		})
+	})
 }
 
 function getPrefixTs() {
-    return `
+	return `
 import {RdbClientBase, RawFilter, Filter, Concurrencies} from 'rdb-client';
 export * from 'rdb-client';
 
@@ -72,28 +72,29 @@ export default rdbClient;`;
 }
 
 function getRdbClientTs(tables) {
-    return `
+	return `
 export interface RdbClient extends RdbClientBase {
     ${getTables()}    
 }
     `;
 
-    function getTables() {
-        let result = ``;
-        for(let name in tables) {
-            let Name = name.substring(0,1).toUpperCase() + name.substring(1);
-            result += 
-`
+	function getTables() {
+		let result = ``;
+		for (let name in tables) {
+			let Name = name.substring(0, 1).toUpperCase() + name.substring(1);
+			result +=
+				`
     ${name}: ${Name}Table;`;
-        }
-        return result;        
-    }
+		}
+		return result;
+	}
 
 }
 
 function getClientIndexTs() {
-    return `
+	return `
     import {RdbClient} from './index.d';
+    import * as d from './index.d';
     import rdbClient from 'rdb-client';
     import tables from '../index';
     
@@ -102,6 +103,7 @@ function getClientIndexTs() {
         return client as RdbClient;
     }
     
+    export import RdbClient = d.RdbClient;
     export default create;`;
 }
 
