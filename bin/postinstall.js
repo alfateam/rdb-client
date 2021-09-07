ea547f11ca16ba1d92a8e7057af40a12f64aa4e3
Lars-Erik Roald <lars.roald@systor.st>
2021-09-07 08:42:57 +0200

    postinstall log

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -3,4 +3,6 @@ let path = require('path');
 let build = require('./build');
 
 let folder = path.normalize(path.join(process.cwd(), '../..'));
+console.log('postinstall');
+console.log(folder);
 build(folder);
